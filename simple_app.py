d53a4ff4e1e50ef50ce1aad592d977c3ecc9930b
Olivia Nordquist <nolivia@umich.edu>
2015-08-04 11:04:56 -0700

    some more pages

--- simple_app.py
+++ simple_app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -9,6 +10,7 @@ def index(name="Olivia"):
 
 @app.route('/add/<int:num1>/<int:num2>')
 def add(num1, num2):
-    return "{} + {} = {}".format(num1, num2, num1 + num2)
+    context = {'num1': num1, 'num2': num2}
+    return render_template("add.html", **context)
 
 app.run(debug=True, port=8000, host='0.0.0.0')
